631d0cadb8740b6ee0a92e4a4cf593a552ef4639
burkbre <burkbre@umich.edu>
2020-04-14 18:56:41 -0400

    added navbar and basic outline for each page

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -10,6 +10,11 @@ import { Player, Players } from '../../models/dashhboard';
 })
 export class DashboardComponent implements OnInit {
   players: Players = new Players();
+  homeActive: boolean = true;
+  resultsActive: boolean = false;
+  myDraftActive: boolean = false;
+  draftSheetActive: boolean = false;
+  userActive: boolean = false;
 
   constructor(
     private dashboardService: DashboardService,
@@ -23,6 +28,50 @@ export class DashboardComponent implements OnInit {
       console.log(res);
       this.players = res;
     });
+  }
+
+  getActive(value: string) {
+    if (value === 'mydraft') {
+      return this.myDraftActive ? 'navbar-active' : '';
+    } else if (value === 'draftsheet') {
+      return this.draftSheetActive ? 'navbar-active' : '';
+    } else if (value === 'results') {
+      return this.resultsActive ? 'navbar-active' : '';
+    }
+  }
+
+  setActive(value: string) {
+    if (value === 'mydraft') {
+      this.myDraftActive = true;
+      this.draftSheetActive = false;
+      this.resultsActive = false;
+      this.homeActive = false;
+      this.userActive = false;
+    } else if (value === 'draftsheet') {
+      this.draftSheetActive = true;
+      this.myDraftActive = false;
+      this.resultsActive = false;
+      this.homeActive = false;
+      this.userActive = false;
+    } else if (value === 'results') {
+      this.resultsActive = true;
+      this.myDraftActive = false;
+      this.draftSheetActive = false;
+      this.homeActive = false;
+      this.userActive = false;
+    } else if (value === 'home') {
+      this.homeActive = true;
+      this.myDraftActive = false;
+      this.draftSheetActive = false;
+      this.resultsActive = false;
+      this.userActive = false;
+    } else if (value === 'user') {
+      this.userActive = true;
+      this.myDraftActive = false;
+      this.draftSheetActive = false;
+      this.resultsActive = false;
+      this.homeActive = false;
+    }
   }
 
 }
